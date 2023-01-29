cce7a517a047d61b2a0a1238a5be2fec5a445525
wivl <68458084+wivl@users.noreply.github.com>
2023-01-29 18:07:19 +0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -1,9 +1,9 @@
 #include "tinytga.h"
 
 typedef struct {
-	uint16_t w;
-	uint16_t h;
+	int w;
+	int h;
 } Point;
 
 /* draw line */
-void draw_line(tt_image* image, Point start, Point end, tt_color color);
+void draw_line(tt_image* image, uint16_t w1, uint16_t h1, uint16_t w2, uint16_t h2, tt_color color);
