5923871eb47450540c4131e31c3fe849cbdc10a6
wivl <68458084+wivl@users.noreply.github.com>
2023-02-02 04:37:22 +0800

    Lesson 1 finished 🎉

--- draw.c
+++ draw.c
@@ -16,41 +16,70 @@ void _swap_u16(uint16_t *a, uint16_t *b) {
 	*b = *a;
 	*a = temp;
 }
+void _swap_int(int *a, int *b) {
+	int temp = *b;
+	*b = *a;
+	*a = temp;
+}
 
-void draw_line(tt_image* image, uint16_t w1, uint16_t h1, uint16_t w2, uint16_t h2, tt_color color) {
+void draw_line(tt_image* image, int w1, int h1, int w2, int h2, tt_color color) {
 	_check_border(image, w1, h1, w2, h2);
 	bool steep = false;
 	if (abs(w1-w2)<abs(h1-h2)) { // if the line is steep, we transpose the image 
-        _swap_u16(&w1, &h1); 
-        _swap_u16(&w2, &h2); 
+        _swap_int(&w1, &h1); 
+        _swap_int(&w2, &h2); 
         steep = true; 
     }
 	if (w1 > w2) {
-		_swap_u16(&w1, &w2);
-		_swap_u16(&h1, &h2);
+		_swap_int(&w1, &w2);
+		_swap_int(&h1, &h2);
 	}
-	int16_t dw = (int16_t)w2 - (int16_t)w1; 
-	int16_t dh = (int16_t)h2 - (int16_t)h1;
+	int dw = w2 - w1; 
+	int dh = h2 - h1;
 	// printf("[DEV]dw: %d\tdh: %d\n", dw, dh);
 	if (w1 == w2) {
 		if (h1 > h2) {
-			_swap_u16(&h1, &h2);
-			_swap_u16(&w1, &w2);
+			_swap_int(&h1, &h2);
+			_swap_int(&w1, &w2);
 		}
-		for (uint16_t h = h1; h <= h2; h++) {
+		for (int h = h1; h <= h2; h++) {
 			tt_set_color(image, w1, h, color);
 		}
 	}
 	float t = (float)dh / (float)dw;
 	// printf("[DEV]t: %f\n", t);
-	for (uint16_t w = w1; w <= w2; w++) {
-		uint16_t h = h1 + t * (w-w1);
+	for (int w = w1; w <= w2; w++) {
+		int h = h1 + t * (w-w1);
 		// printf("[DEV]w: %u\th: %u\n", w, h);
 		if (steep) {
-			tt_set_color(image, h, w, color);
+			// printf("[DEV]w: %u h: %u\n", w, h);
+			tt_set_color(image, h-1, w-1, color);
 		} else {
-			tt_set_color(image, w, h, color);
+			// printf("[DEV]w: %u h: %u\n", w, h);
+			tt_set_color(image, w-1, h-1, color);
 		}
 	}
 }
 
+void line(int x0, int y0, int x1, int y1, tt_image *image, tt_color color) {
+    bool steep = false;
+    if (abs(x0-x1)<abs(y0-y1)) {
+        _swap_int(&x0, &y0);
+        _swap_int(&x1, &y1);
+        steep = true;
+    }
+    if (x0>x1) {
+        _swap_int(&x0, &x1);
+        _swap_int(&y0, &y1);
+    }
+
+    for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
+        int y = y0*(1.-t) + y1*t;
+        if (steep) {
+            tt_set_color(image, y-1, x-1, color);
+        } else {
+            tt_set_color(image, x-1, y-1, color);
+        }
+    }
+}
--- draw.h
+++ draw.h
@@ -2,4 +2,6 @@
 
 
 /* draw line */
-void draw_line(tt_image* image, uint16_t w1, uint16_t h1, uint16_t w2, uint16_t h2, tt_color color);
+void draw_line(tt_image* image, int w1, int h1, int w2, int h2, tt_color color);
+
+void line(int x0, int y0, int x1, int y1, tt_image *image, tt_color color);
