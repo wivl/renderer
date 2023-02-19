5f15f6ca06a036955561eab919bb511101e6c672
wivl <68458084+wivl@users.noreply.github.com>
2023-02-19 21:03:17 +0800

    Lesson 4 finished! 🎉 (with texture)

--- draw.c
+++ draw.c
@@ -1,6 +1,8 @@
 #include "draw.h"
 #include "geometry.h"
 #include "tinytga.h"
+#include "tinyobj.h"
+#include "matrix.h"
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -10,7 +12,9 @@
 #include <limits.h>
 #include <float.h>
 
-void _check_border(tt_image* image, uint16_t w1, uint16_t h1, uint16_t w2, uint16_t h2) {
+
+void _check_border(tt_image* image, uint16_t w1, uint16_t h1,
+        uint16_t w2, uint16_t h2) {
 	assert(w1 <= image->width && h1 <= image->height);
 	assert(w2 <= image->width && h2 <= image->height);
 }
@@ -26,21 +30,39 @@ void _swap_int(int *a, int *b) {
 	*a = temp;
 }
 
-void draw_line(tt_image* image, int w1, int h1, int w2, int h2, tt_color color) {
+void _vec3i_swap(Vec3i *a, Vec3i *b) {
+    Vec3i temp = *b;
+    *b = *a;
+	*a = temp;
+}
+
+void _vec2i_swap(Vec2i *a, Vec2i *b) {
+    Vec2i temp = *b;
+    *b = *a;
+	*a = temp;
+}
+
+void draw_line(tt_image* image, int w1, int h1, int w2, int h2,
+        tt_color color) {
 	_check_border(image, w1, h1, w2, h2);
+
 	bool steep = false;
-	if (abs(w1-w2)<abs(h1-h2)) { // if the line is steep, we transpose the image 
-		_swap_int(&w1, &h1); 
+
+    // if the line is steep, transpose the image.
+	if (abs(w1-w2)<abs(h1-h2)) {
+        _swap_int(&w1, &h1); 
 		_swap_int(&w2, &h2); 
 		steep = true; 
 	}
+
+    // if w1 is on the left, swap.
 	if (w1 > w2) {
 		_swap_int(&w1, &w2);
 		_swap_int(&h1, &h2);
 	}
+
 	int dw = w2 - w1; 
 	int dh = h2 - h1;
-	// printf("[DEV]dw: %d\tdh: %d\n", dw, dh);
 	if (w1 == w2) {
 		if (h1 > h2) {
 			_swap_int(&h1, &h2);
@@ -51,15 +73,12 @@ void draw_line(tt_image* image, int w1, int h1, int w2, int h2, tt_color color) 
 		}
 	}
 	float t = (float)dh / (float)dw;
-	// printf("[DEV]t: %f\n", t);
+
 	for (int w = w1; w <= w2; w++) {
 		int h = h1 + t * (w-w1);
-		// printf("[DEV]w: %u\th: %u\n", w, h);
 		if (steep) {
-			// printf("[DEV]w: %u h: %u\n", w, h);
 			tt_set_color(image, h-1, w-1, color);
 		} else {
-			// printf("[DEV]w: %u h: %u\n", w, h);
 			tt_set_color(image, w-1, h-1, color);
 		}
 	}
@@ -110,7 +129,8 @@ Vec3f _barycentric(Vec3f *pts, Vec3f P) {
 }
 
 
-void draw_triangle(tt_image *image, Vec3f *pts, float *zbuffer, tt_color color) {
+void draw_triangle(tt_image *image, Vec3f *pts, float *zbuffer,
+        tt_color color) {
 	// bounding box ----------> x
 	//              |  min
 	//              |
@@ -119,8 +139,10 @@ void draw_triangle(tt_image *image, Vec3f *pts, float *zbuffer, tt_color color) 
 
 	Vec2i bboxmin = { .x = image->width-1, .y = image->height-1 };
 	Vec2i bboxmax = { .x = 0, .y = 0 };
+    // canvas
 	Vec2i clamp = { .x = image->width-1, .y = image->height-1 };
 
+    // calculate bounding boxes
 	for (int i = 0; i < 3; i++) {
 		bboxmin.x = (int)fmaxf(0, fminf((float)bboxmin.x, (float)pts[i].x));
 		bboxmin.y = (int)fmaxf(0, fminf((float)bboxmin.y, (float)pts[i].y));
@@ -129,32 +151,94 @@ void draw_triangle(tt_image *image, Vec3f *pts, float *zbuffer, tt_color color) 
 		bboxmax.y = (int)fminf((float)clamp.y, fmaxf((float)bboxmax.y, (float)pts[i].y));
 	}
 
-	// printf("[%d]min: (%d, %d)\n", ++count, bboxmin.x, bboxmin.y);
-	// printf("[%d]max: (%d, %d)\n", count, bboxmax.x, bboxmax.y);
-	// printf("[%d]clamp: (%d, %d)\n", count, clamp.x, clamp.y);
-
 	Vec3f P;
 	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
 		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+            // get barycentric
 			Vec3f bc_screen = _barycentric(pts, P);
-			// printf("[%d]bc_screen (%f, %f, %f)\n", ++count, bc_screen.x, bc_screen.y, bc_screen.z);
-			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) {
-				continue;
-			}
+
+            // if the point is not in the triangle, skip.
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0)
+                continue;
+
+            // calculate zbuffer value
 			P.z = 0;
 			P.z += pts[0].z * bc_screen.x;
 			P.z += pts[1].z * bc_screen.y;
 			P.z += pts[2].z * bc_screen.z;
+
+            // if the pixel is on top, draw and update zbuffer.
 			if (zbuffer[(int)((P.x-1)+(P.y-1)*image->width)] < P.z) {
 				zbuffer[(int)((P.x-1)+(P.y-1)*image->width)] = P.z;
 				tt_set_color(image, P.x-1, P.y-1, color);
 			}
-			// printf("[DEV]u = (%f, %f, %f)\n", bc_screen.x, bc_screen.y, bc_screen.z);
-			// printf("[DEV]P.x: %d\tP.y: %d\tin\n", P.x, P.y);
 		}
 
 	}
 }
+
+Matrix viewpoint(int x, int y, int w, int h, int depth) {
+    Matrix m = m_identity(4);
+    m.data[0*m.cols+3] = x+w/2.f;
+    m.data[1*m.cols+3] = y+h/2.f;
+    m.data[2*m.cols+3] = depth/2.f;
+
+    m.data[0*m.cols+0] = w/2.f;
+    m.data[1*m.cols+1] = h/2.f;
+    m.data[2*m.cols+2] = depth/2.f;
+    return m;
+}
+
+
+void draw_triangle_texture(Vec3i t0, Vec3i t1, Vec3i t2,
+        Vec2i uv0, Vec2i uv1, Vec2i uv2, tt_image *image,
+        float intensity, int *zbuffer, tobj_model *model) {
+    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+    if (t0.y>t1.y) { _vec3i_swap(&t0, &t1); _vec2i_swap(&uv0, &uv1); }
+    if (t0.y>t2.y) { _vec3i_swap(&t0, &t2); _vec2i_swap(&uv0, &uv2); }
+    if (t1.y>t2.y) { _vec3i_swap(&t1, &t2); _vec2i_swap(&uv1, &uv2); }
+
+    int total_height = t2.y-t0.y;
+    // 从三角形的最低点到最高点
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+
+        // Vec3i A   =               t0  + Vec3f(t2-t0  )*alpha;
+        // Vec3i B   = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
+        // Vec2i uvA =               uv0 +      (uv2-uv0)*alpha;
+        // Vec2i uvB = second_half ? uv1 +      (uv2-uv1)*beta : uv0 +      (uv1-uv0)*beta;
+
+        Vec3i A = vec3i_add(t0, vec3f_to_i(vec3f_multiply_f(vec3i_to_f(vec3i_minus(t2, t0)), alpha)));
+        Vec3i B = second_half ? vec3i_add(t1, vec3f_to_i(vec3f_multiply_f(vec3i_to_f(vec3i_minus(t2, t1)), beta))) : 
+            vec3i_add(t0, vec3f_to_i(vec3f_multiply_f(vec3i_to_f(vec3i_minus(t1, t0)), beta)));
+        Vec2i uvA = vec2i_add(uv0, vec2i_multiply(vec2i_minus(uv2, uv0), alpha));
+        Vec2i uvB = second_half ? vec2i_add(uv1, vec2i_multiply(vec2i_minus(uv2, uv1), beta)) : 
+            vec2i_add(uv0, vec2i_multiply(vec2i_minus(uv1, uv0), beta));
+        
+        if (A.x>B.x) { _vec3i_swap(&A, &B); _vec2i_swap(&uvA, &uvB); }
+        for (int j=A.x; j<=B.x; j++) {
+            float phi = B.x==A.x ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
+            // Vec3i P = Vec3f(A) + Vec3f(B-A)*phi;
+            Vec3i P = vec3f_to_i(vec3f_add(vec3i_to_f(A), vec3f_multiply_f(vec3i_to_f(vec3i_minus(B, A)), phi)));
+            // Vec2i uvP =     uvA +   (uvB-uvA)*phi;
+            Vec2i uvP = vec2i_add(uvA, vec2i_multiply(vec2i_minus(uvB, uvA), phi));
+            int idx = P.x+P.y*image->width;
+            if (zbuffer[idx]<P.z) {
+                zbuffer[idx] = P.z;
+                tt_color color = tobj_diffuse(model, uvP);
+                color.r *= intensity;
+                color.g *= intensity;
+                color.b *= intensity;
+                tt_set_color(image, P.x, P.y, color);
+            }
+        }
+    }
+}
+
+
 
 Vec3f world2screen(Vec3f v, const int width, const int height) {
 	Vec3f result = {
--- test.c
+++ test.c
@@ -2,19 +2,23 @@
 #include "geometry.h"
 #include "draw.h"
 #include "tinyobj.h"
+#include "matrix.h"
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <float.h>
+#include <limits.h>
+
 
 #define IMAGE_WIDTH 800
 #define IMAGE_HEIGHT 800
 
 int main(void) {
 	srand(time(NULL));
-	tobj_model *model = tobj_load_model("african_head.obj");
+	tobj_model *model = tobj_load_model("african_head.obj", TEXTURE_TGA);
+    tobj_load_texture(model, "african_head_diffuse.tga");
 
 	tt_color black = tt_make_color(0xFF000000);
 	tt_color white = tt_make_color(0xFFFFFFFF);
@@ -23,31 +27,54 @@ int main(void) {
 
 	
 	Vec3f light_dir = { .x=0, .y=0, .z=-1 };
+	Vec3f camera = { .x=0, .y=0, .z=3 };
 
-	float *zbuffer = (float*)malloc(sizeof(float)*IMAGE_WIDTH*IMAGE_HEIGHT);
+	int *zbuffer = (int*)malloc(sizeof(int)*IMAGE_WIDTH*IMAGE_HEIGHT);
 	for (int i = 0; i < IMAGE_HEIGHT*IMAGE_WIDTH; i++) {
-		zbuffer[i] = -FLT_MAX;
+		zbuffer[i] = INT_MIN;
 	}
 
-	for (int i=0; i<model->f_capp; i++) { // 每个面
-        int *face = model->faces[i];
-		Vec3f pts[3];
-		Vec3f world_coords[3];
-        for (int j=0; j<3; j++) { // 每个点
-			world_coords[j] = model->verts[face[j]-1];
-			pts[j] = world2screen(model->verts[face[j]-1], IMAGE_WIDTH, IMAGE_HEIGHT);
+	Matrix Projection = m_identity(4);
+    Matrix Viewpoint = viewpoint(IMAGE_WIDTH/8, IMAGE_HEIGHT/8, 
+            IMAGE_WIDTH*3/4, IMAGE_HEIGHT*3/4, 255);
+    m_set(&Projection, 3, 2, -1.f/camera.z);
+    
+    for (int i=0; i<model->f_capp; i++) {       // 每个面
+        int *face = tobj_get_face(model, i);
+        Vec3i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j=0; j<3; j++) {               // 每个点
+            Vec3f v = model->verts[face[j]];
+            screen_coords[j] =  vec3f_to_i(m_matrix_to_vec3f(m_multiply(m_multiply(Viewpoint, Projection), m_vec3f_to_matrix(v))));
+            world_coords[j]  = v;
         }
-		Vec3f n = vec3f_cross(vec3f_minus(world_coords[2], world_coords[0]), vec3f_minus(world_coords[1], world_coords[0]));
-		vec3f_normalize(&n, 1);
-		float intensity = vec3f_multiply_v(n, light_dir);
-		// tt_color color = { .a = 0xFF, .r = rand()%255, .g = rand()%255, .b = rand()%255};
-		if (intensity > 0) {
-			tt_color color = { .a = 0xFF, .r = intensity*255, .g = intensity*255, .b = intensity*255};
-			draw_triangle(image, pts, zbuffer, color);
-		}
-		
+        Vec3f n = vec3f_cross(vec3f_minus(world_coords[2], world_coords[0]), vec3f_minus(world_coords[1], world_coords[0]));
+        vec3f_normalize(&n, 1);
+        float intensity = vec3f_multiply_v(n, light_dir);
+        if (intensity>0) {
+            Vec2i uv[3];
+            for (int k=0; k<3; k++) {
+                uv[k] = tobj_get_uv(model, i, k);
+            }
+            draw_triangle_texture(screen_coords[0], screen_coords[1], screen_coords[2], uv[0], uv[1], uv[2], image, intensity, zbuffer, model);
+        }
     }
-
+    { // dump z-buffer (debugging purposes only)
+        tt_image *zbimage = tt_create(IMAGE_WIDTH, IMAGE_HEIGHT, black);
+        for (int i=0; i<IMAGE_WIDTH; i++) {
+            for (int j=0; j<IMAGE_HEIGHT; j++) {
+                tt_color color = {
+                    .a = 0xFF,
+                    .r = zbuffer[i+j*IMAGE_WIDTH],
+                    .g = zbuffer[i+j*IMAGE_WIDTH],
+                    .b = zbuffer[i+j*IMAGE_WIDTH],
+                };
+                tt_set_color(zbimage, i, j, color);
+            }
+        }
+        tt_flip_vertically(zbimage);
+        tt_save(zbimage, "zbuffer.tga");
+    }
 	
 	tt_flip_vertically(image);
 	tt_save(image, "saved.tga");
