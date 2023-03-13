c1a8e60999e9a2a5e1d3a8c9b3ea83b828da2e8f
wivl <68458084+wivl@users.noreply.github.com>
2023-03-13 23:00:06 +0800

    Clean and add comments

--- shader.h
+++ shader.h
@@ -7,19 +7,22 @@
 #include "tinyobj.h"
 
 typedef struct {
-    Vec3f light_dir;
-    Vec3f eye;
-    Vec3f center;
-    Vec3f up;
+    Vec3f light_dir;    // light source position
+    Vec3f eye;          // camera position
+    Vec3f center;       // TODO: not understand yet
+    Vec3f up;           // camera up direction
 
-    Matrix uniform_M;   //  4x4 Projection*ModelView
-    Matrix uniform_MIT; // 4x4 (Projection*ModelView).invert_transpose()
-    Matrix uniform_Mshadow;   //  4x4 transform framebuffer screen coordinates to shadowbuffer screen coordinates
+    Matrix uniform_M;           // 4x4 Projection*ModelView
+    Matrix uniform_MIT;         // 4x4 (Projection*ModelView).invert_transpose()
+    Matrix uniform_Mshadow;     // 4x4 matrix, transform framebuffer screen coordinates to shadowbuffer screen coordinates
 
-    Vec3f varying_intensity; // intensity of 3 vertexes
-    Matrix varying_uv;  // a 2x3 matrix. 2 rows for u and v, 3 columns (one per vertex)
-    Matrix varying_tri; // 3x3, for depth shader
-    Matrix final_matrix;// combination
+    tt_image *aoimage;  // ambient occlusion texture
+
+    Vec3f varying_intensity;    // intensity of 3 vertices
+    Matrix varying_uv;          // a 2x3 matrix. 2 rows for u and v, 3 columns (one per vertex)
+    Matrix varying_tri;         // 3x3, for depth shader
+    Matrix varying_tri4;        // 4x3, for ao shader
+    // Matrix final_matrix;        // combination
 } Shader;
 
 #define DEPTH 2000
@@ -52,52 +55,61 @@ void S_set_projection(float coeff);
 // model to world
 void S_set_modelview(Vec3f eye, Vec3f center, Vec3f up);
 
-void S_get_final_matrix(Shader *shader);
+void S_init_shader(Shader *shader);
 
+
+// No texture
 Vec4f S_vertex_shader(Shader *shader, tobj_model *model, int iface, int nthvert);
-
-Vec4f S_vertex_shader_texture(Shader *shader, tobj_model *model, int iface, int nthvert);
-
 bool S_fragment_shader(Shader *shader, Vec3f bar, tt_color *color);
-
-bool S_fragment_shader_texture(Shader *shader, tobj_model *model, Vec3f bar, tt_color *color);
-
 void S_draw_triangle(Shader *shader, tt_image *image, tobj_model *model,
         Vec4f *pts, int *zbuffer);
 
+
+// texture
+Vec4f S_vertex_shader_texture(Shader *shader, tobj_model *model, int iface, int nthvert);
+bool S_fragment_shader_texture(Shader *shader, tobj_model *model, Vec3f bar, tt_color *color);
 void S_draw_triangle_texture(Shader *shader, tt_image *image, tobj_model *model,
         Vec4f *pts, int *zbuffer);
 
+
+
+// texture + normalmapping
 Vec4f S_vertex_shader_normalmapping(Shader *shader, tobj_model *model, int iface, int nthvert);
-
 bool S_fragment_shader_normalmapping(Shader *shader, tobj_model *model, Vec3f bar, tt_color *color);
-
 void S_draw_triangle_normalmapping(Shader *shader, tt_image *image, tobj_model *model,
         Vec4f *pts, int *zbuffer);
 
 
 
+// texture + normalmapping + specularmapping
 Vec4f S_vertex_shader_specular(Shader *shader, tobj_model *model, int iface, int nthvert);
-
 bool S_fragment_shader_specular(Shader *shader, tobj_model *model, Vec3f bar, tt_color *color);
-
 void S_draw_triangle_specular(Shader *shader, tt_image *image, tobj_model *model,
         Vec4f *pts, int *zbuffer);
 
+
+
+// depth shader for shadow
 Vec4f S_vertex_shader_depth(Shader *shader, tobj_model *model, int iface, int nthvert);
-
 bool S_fragment_shader_depth(Shader *shader, tobj_model *model, Vec3f bar, tt_color *color);
-
 void S_draw_triangle_depth(Shader *shader, tt_image *image, tobj_model *model,
         Vec4f *pts, float *zbuffer);
 
-// TODO: shadow shader
+
+
+// shadow shader
 Vec4f S_vertex_shader_shadow(Shader *shader, tobj_model *model, int iface, int nthvert);
-
 bool S_fragment_shader_shadow(Shader *shader, tt_image *image, tobj_model *model, float *shadowbuffer, Vec3f bar, tt_color *color);
-
 void S_draw_triangle_shadow(Shader *shader, tt_image *image, tobj_model *model,
         Vec4f *pts, float *zbuffer, float *shadowbuffer);
+
+
+
+// TODO: AO SHADER
+Vec4f S_vertex_shader_AO(Shader *shader, tobj_model *model, int iface, int nthvert);
+bool S_fragment_shader_AO(Shader *shader, Vec3f fragcoord, Vec3f bar, tt_color *color);
+void S_draw_triangle_AO(Shader *shader, tt_image *image, tobj_model *model,
+        Vec4f *pts, float *zbuffer);
 
 
 #endif
